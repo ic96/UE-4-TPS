01b7e6b919fbecbaf4944c5a97eb67f88fc06d1e
Ian <ian.chan96@hotmail.com>
2018-09-23 15:55:51 -0700

    Scripts

---
+++ Source/TPS/TPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATPSGameMode();
+};
+
+
+
